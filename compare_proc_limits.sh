8def985273df03635d2c8905a889ed046e522b66
markcurtis1970 <mark.curtis@datastax.com>
2015-07-08 17:43:04 +0100

    updated proc limits script

--- compare_proc_limits.sh
+++ compare_proc_limits.sh
@@ -6,38 +6,42 @@
 # Author Erick Ramirez, 2015 Jul 02
 #
 
-# assume we are in the "nodes" directory
-node0=`ls | head -1`
+# Added function to search for files using
+# find command.
+function checkForFiles {
+    # validate we can find at least 1 file
+    files=$(find . -name "$1" -type f)
 
-# validate we can find at least 1 process_limits file
-plfiles=$(find . -name "process_limits" -type f)
+    # check if no files found
+    if [ -z "$files" ]
+    then
+        echo "Couldn't find any files named $1 from here. Exiting"
+        exit 1
+    fi
 
-#if no files found
-if [ $plfiles = "" ]
-then
-    echo "USAGE - Please run script in the nodes directory of a Diagnostics Report"
-    exit 1
-fi
+    # get the first directory
+    for file in $files
+    do
+        node0=$file
+        break
+    done
+}
 
-#get the first directory
-for plfile in $plfiles
-do
-    node0=$(dirname $plfile)
-    break
-done
+# Check for the files first
+checkForFiles process_limits
 
 #get the header from the first file
-if [ -r $node0/process_limits ]
+if [ -r $node0 ]
 then
     # at least 1 file is readable
-    header=`grep Limit $node0/process_limits`
+    header=`grep Limit $node0`
 else
     echo "USAGE - Please run script in the nodes directory of a Diagnostics Report"
     exit 1
 fi
 
 # get list of limits
-grep -v ^Limit $node0/process_limits | while read line
+grep -v ^Limit $node0 | while read line
 do
     limit=`echo "$line" | sed -e 's/  .*$//'`
 #echo "DEBUG >>>>> limit=[$limit]"
@@ -47,7 +51,7 @@ do
     echo "-----------------------------------------------------------------------------------------------"
 
     # iterate through all nodes
-    for node in $plfiles
+    for node in $files
     do
         printf "%15s - " $node
         grep "$limit" $node
