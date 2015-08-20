c0e5e2a457a25eddb655e0611ecabdc7b68e109c
Erick Ramirez <erick.ramirez@datastax.com>
2015-08-20 12:01:02 +1000

    Changed log message date/time tokenisation to use sed instead of awk

--- count_entries_per_hour.sh
+++ count_entries_per_hour.sh
@@ -4,23 +4,25 @@
 # in the Cassandra system.log.
 #
 # Author Erick Ramirez, 2015 Mar 31
+# Updated by Erick Ramirez, 2015 Aug 20 - changed the way date/time is getting tokenized to account for variations in log entries
 #
-
-#q=ParNew
-#for node in *; do echo "===== $node ====="; log=$node/system-20150317.log; for x in `grep "$q" $log | awk '{print $4}' | cut -d: -f1 | sort -u`; do y=`grep "$q" $log | grep -c "2015-03-17 $x:"`; echo "${x}:00 - $y"; done; echo ""; done
 
 # validate arguments
 if [ "$#" -ne 2 ]
 then
     echo "ERROR - Usage: `basename $0` <query_string> <system_log>"
     exit 1
+elif [ ! -r $2 ]
+then
+    echo "ERROR - Log file [$2] not readable"
+    exit 2
 else
     query_string="$1"
     system_log="$2"
 fi
 
 # obtain the unique list of hours where there's a match in the log
-grep "$query_string" $system_log | awk '{print $3, $4}' | cut -d: -f1 | sort -u | while read hour
+grep "$query_string" $system_log | sed -e 's/.* \([0-9]*-[0-9]*-[0-9]* [0-9]*\):.*/\1/' | sort -u | while read hour
 do
     # count number of occurences of query_string for given hour
     count=`grep "$query_string" $system_log | grep -c "$hour:"`
