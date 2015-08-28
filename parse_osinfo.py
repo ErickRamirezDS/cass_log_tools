10d88932b3a3dfd977ae94cf28cc40e66ab553fd
markcurtis1970 <mark.curtis@datastax.com>
2015-08-28 11:45:30 +0100

    Adding couple more

---
+++ parse_osinfo.py
@@ -0,0 +1,15 @@
+# parses the node_info.json file
+# so the values for each node are
+# displayed side by side
+
+import os
+import json
+
+# load the file
+data = json.loads(open("./disk.json").read())
+
+# loop through the first item only to get only one lot of items
+for items in data:
+ 
+    # print out the values for each node
+    print items,": ",data[items]
