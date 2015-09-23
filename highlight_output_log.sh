aa676aee92c320d0ba5312d98c65e3d658894573
Erick Ramirez <erick.ramirez@datastax.com>
2015-09-22 22:24:23 -0700

    Added 'tail -1' to only report last occurrence of multiple entries in log

--- highlight_output_log.sh
+++ highlight_output_log.sh
@@ -3,6 +3,7 @@
 # Gets a list of support-relevant entries from the output.log
 #
 # Author Erick Ramirez, 2015 Jul 14
+# Updated 2015 Sep 22, Erick Ramirez - Added "tail -1" to end of grep for multiple entries in logs
 #
 
 
@@ -18,31 +19,31 @@ then
 fi
 
 echo "===== DSE version ====="
-grep "DSE version" */logs/cassandra/output.log 2> /dev/null
+grep "DSE version" */logs/cassandra/output.log 2> /dev/null | tail -1
 echo ""
 
 echo "===== Cassandra version ====="
-grep "Cassandra version" */logs/cassandra/output.log 2> /dev/null
+grep "Cassandra version" */logs/cassandra/output.log 2> /dev/null | tail -1
 echo ""
 
 echo "===== JNA ====="
-grep "JNA" */logs/cassandra/output.log 2> /dev/null
+grep "JNA" */logs/cassandra/output.log 2> /dev/null | tail -1
 echo ""
 
 echo "===== Heap size ====="
-grep "Heap size" */logs/cassandra/output.log 2> /dev/null
+grep "Heap size" */logs/cassandra/output.log 2> /dev/null | tail -1
 echo ""
 
 echo "===== Java version ====="
-grep "JVM vendor" */logs/cassandra/output.log 2> /dev/null
+grep "JVM vendor" */logs/cassandra/output.log 2> /dev/null | tail -1
 echo ""
 
 echo "===== Workload ====="
-grep "etting my workload" */logs/cassandra/output.log 2> /dev/null
+grep "etting my workload" */logs/cassandra/output.log 2> /dev/null | tail -1
 echo ""
 
 echo "===== Snitch ====="
-grep "Snitch" */logs/cassandra/output.log 2> /dev/null | grep Initialized
+grep "Snitch" */logs/cassandra/output.log 2> /dev/null | grep Initialized | tail -1
 echo ""
 
 echo "NOTE - For the output to be readable, run the rename_node_dirs.sh script."
