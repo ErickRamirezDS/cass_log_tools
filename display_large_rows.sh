f39fc2896fa164ddaa19df1a32ab250c67336382
Erick Ramirez <erick.ramirez@datastax.com>
2016-01-07 16:15:40 +1100

    Updates as a result of changes in CASSANDRA-9643

--- display_large_rows.sh
+++ display_large_rows.sh
@@ -3,10 +3,11 @@
 # A script for displaying entries in logs with large rows.
 #
 # Author Erick Ramirez, 2015 Apr 14
+# Updated 2016 Jan 07 - Changed pattern from "large row" to "Compacting large " as a result of CASSANDRA-9643
 #
 
 # validate input file
-if [ "$1" == "" ]
+if [ "$1" = "" ]
 then
     echo "ERROR - Usage: `basename $0` <system_log>"
     exit 1
@@ -16,7 +17,7 @@ else
 fi
 
 # check for minimum size
-if [ "$2" == "" ]
+if [ "$2" = "" ]
 then
     # minimum size not set so default to 1MB
     min_size_bytes=999999
@@ -24,10 +25,11 @@ else
     min_size_bytes=$2
 fi
 
-grep "large row" $system_log | while read line
+grep "Compacting large " $system_log | while read line
 do
     # extract the row size
-    row_size=`echo $line | awk '{print $(NF-2)}' | sed -e 's/(//'`
+    #row_size=`echo $line | awk '{print $(NF-2)}' | sed -e 's/(//'`
+    row_size=`echo $line | sed -e 's/.* (\([0-9]*\) bytes.*/\1/'`
 
     if [ $row_size -gt $min_size_bytes ]
     then
