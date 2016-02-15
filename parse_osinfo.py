1ca4de2b83dd83a3a48d64111ba6da5fc556d2e9
markcurtis1970 <mark.curtis@datastax.com>
2016-02-15 10:11:10 +0000

    Adding parse_osinfo.py

--- parse_osinfo.py
+++ parse_osinfo.py
@@ -5,12 +5,53 @@
 
 import os
 import json
+import re
+import sys
 
-# load the file
-data = json.loads(open("./disk.json").read())
+# Setup variables etc
+path = '.'
 
-# loop through the first item only to get only one lot of items
-for items in data:
+def findAndParse(name):
+    # Reset variables etc
+    found = False
+    data = { }
+    # Use os.walk to find all the directories under the path
+    for dirname, dirnames, filenames in os.walk(path):
+        for file in filenames:
+            # Check if the file name matches
+            matched = re.match(name, file, re.M)
+            if matched:
+                # Set flag
+                found = True
+                # load the file
+                currentfile = dirname+'/'+file
+                # load the data into a dictionary
+                data[currentfile] = json.loads(open(currentfile).read())
+                # break once complete (theres only one file per diretory)
+                break
+
+    # If the file is not found anywhere then exit function
+    # So we can continue with the next file
+    if not found:
+        print '\nUnable to find file named ' + name + '\n'
+        return
+
+    # loop through the first item only to get only one lot of items
+    for items in data.values()[0]:
+
+        # loop through the first level of items (i.e. all nodes)
+        for node in data:
  
-    # print out the values for each node
-    print items,": ",data[items]
+            # print out the values for each node
+            print "node: ", node, items, data[node][items]
+
+# Add your files in here you need to parse
+# Comment out the ones you dont want
+findAndParse('os-info.json')
+findAndParse('cpu.json')
+findAndParse('memory.json')
+findAndParse('disk.json')
+findAndParse('disk_space.json')
+#findAndParse('load_avg.json') # parsing this one throws an error so leaving out for now
+
+
