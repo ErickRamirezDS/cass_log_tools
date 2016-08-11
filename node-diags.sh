95ae06b9c0d02ab82188eb2b80231f19cfb9cdee
markcurtis1970 <mark.curtis@datastax.com>
2016-08-11 12:40:59 +0100

    add variable for host IP and include logs

--- node-diags.sh
+++ node-diags.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 #
+# The xargs removes any leading / training spaces
 
-# Run commands, redirect to individual files
-nodetool status > `hostname -i`-nodetool_status.out
-nodetool describecluster > `hostname -i`-nodetool_describecluster.out
-nodetool gossipinfo > `hostname -i`-nodetool_gossipinfo.out
-nodetool info > `hostname -i`-nodetool_info.out
-nodetool cfstats > `hostname -i`-nodetool_cfstats.out
-nodetool tpstats > `hostname -i`-nodetool_tpstats.out
-nodetool compactionstats > `hostname -i`-nodetool_compactionstats.out
-nodetool netstats > `hostname -i`-nodetool_netstats.out
-nodetool ring > `hostname -i`-nodetool_ring.out
-dsetool ring > `hostname -i`-dsetool_ring.out
-java -version 2> `hostname -i`-java_version.out # always writes to stderr
-df -h > `hostname -i`-df_h.out
+HOST=$(hostname -I | xargs)
+
+# Grab logs etc
+
+cp /var/log/cassandra/system.log ./$HOST-system.log
+cp /var/log/cassandra/output.log ./$HOST-output.log
+
+# Grab nodetool commands
+
+nodetool status > ./$HOST-nodetool_status.out
+nodetool describecluster > ./$HOST-nodetool_describecluster.out
+nodetool gossipinfo > ./$HOST-nodetool_gossipinfo.out
+nodetool info > ./$HOST-nodetool_info.out
+nodetool cfstats > ./$HOST-nodetool_cfstats.out
+nodetool tpstats > ./$HOST-nodetool_tpstats.out
+nodetool compactionstats > ./$HOST-nodetool_compactionstats.out
+nodetool netstats > ./$HOST-nodetool_netstats.out
+nodetool ring > ./$HOST-nodetool_ring.out
+dsetool ring > ./$HOST-dsetool_ring.out
+java -version 2> ./$HOST-java_version.out # always writes to stderr
+df -h > ./$HOST-df_h.out
 
 # Tar up the files
-tar zcf `hostname -i`-files.tar.gz `hostname -i`-*
+tar zcf ./$HOST-files.tar.gz ./$HOST-*
